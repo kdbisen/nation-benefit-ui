cba3f7493b6df39023d386ec367f18c4a11dde0f
Kuldeep <kuldeep.bisen1@gmail.com>
2022-09-18 18:53:01 +0530

    init

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="about">About</Link>
+      </nav>
+    </div>
+  );
+}
---
+++ src/components/Invoices.tsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
+function Invoices() {
+  let navigate = useNavigate();
+  return (
+    <div>
+       
+    </div>
+  );
+}
